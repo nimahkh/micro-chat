a7b06c69e3f828c11bb9ac11e75fad28f811de5d
nimatrengo <88784130+nimatrengo@users.noreply.github.com>
2021-10-31 02:27:21 +0300

    Add getting and writing files and append

--- src/Console/handlers/generateFile.ts
+++ src/Console/handlers/generateFile.ts
@@ -8,6 +8,16 @@ class GenerateFile extends Commander {
 
     private logger;
 
+    /**
+     * find all imports
+     */
+    private importRegex : string = "^import.+;$"
+
+    /**
+     * find all exports
+     */
+    private exportRegex : string = "(?<=export {)(.*)(?=})"
+
     constructor() {
         super();
         this.logger = new Logger()
@@ -20,6 +30,7 @@ class GenerateFile extends Commander {
     public makeFile(file_name: string, path: string, data : string, type: Types, callback ? : ()=> void): void {
         const filePath = _path.join(process.env.PWD!, path!)
         const file = `${filePath}/${file_name}${type}.ts`;
+
         if(!this.fileExists(file)) {
             fs.writeFile(file, data, (err) => {
                 if (err) throw err;
@@ -29,6 +40,28 @@ class GenerateFile extends Commander {
             return
         }
         this.logger.error(`${type}  Exists`)
+    }
+
+    public readFile(file_path: string): string {
+        return  fs.readFileSync(file_path, 'utf-8')
+    }
+
+    public writeFile(file_path: string, data : string): void {
+        fs.writeFileSync(file_path , data);
+    }
+
+    public appendIntoImports(data: string, toAdd : string): string[] {
+        const findImports = new RegExp(this.importRegex , 'gim')
+        const lines : string[] = Array.from(data.matchAll(findImports), m => `${m[0]}`) || []
+        lines.push(toAdd)
+        return lines
+    }
+
+    public appendIntoExport(data: string, toAdd : string): string[] {
+        const findImports = new RegExp(this.exportRegex , 'gim')
+        const lines : string[] = data.match(findImports) || []
+        lines.push(toAdd)
+        return lines
     }
 }
 
