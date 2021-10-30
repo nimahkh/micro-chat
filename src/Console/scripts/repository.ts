8b648e1e82466f2f262ac383220e144821a55c7a
nimatrengo <88784130+nimatrengo@users.noreply.github.com>
2021-10-31 02:25:19 +0300

    Add appending repository inside index.ts

--- src/Console/scripts/repository.ts
+++ src/Console/scripts/repository.ts
@@ -4,14 +4,29 @@ import repositoryDTO from "../dto/Repository";
 
 export class Repository extends GenerateFile {
 
-    private repo_path = "./src/HTTP/repositories"
+    private repo_path: string = "./src/HTTP/repositories"
+    private repo_name: string = ""
 
     public make(name? :string){
         const options : IOption[] = [
             {flags : '-n, --name <name>', description: 'repository name'},
         ]
         const args = this.programs(options).parse(process.argv)
-        const repo_name = name || args.opts().name
-        this.makeFile(repo_name, this.repo_path, repositoryDTO(repo_name), "Repository")
+        this.repo_name = name || args.opts().name
+        this.makeFile(this.repo_name, this.repo_path, repositoryDTO(this.repo_name), "Repository", this.appendRepositoryToIndex.bind(this))
+    }
+
+    protected appendRepositoryToIndex(){
+        const path = `${this.repo_path}/index.ts`
+        const readFile = this.readFile(path)
+        const importLineToAdd = `import ${this.repo_name}Repository from './${this.repo_name}Repository';`
+        const exportToAdd = `${this.repo_name}Repository`
+        const imports : string[] = this.appendIntoImports(readFile, importLineToAdd)
+        const exports : string[] = this.appendIntoExport(readFile, exportToAdd)
+        const combineImports = imports.join("\n")
+        const combineExports = `export {${exports.join(",")} }`
+        const combineAll = `${combineImports}\n\n${combineExports}`
+
+        this.writeFile(path, combineAll)
     }
 }
